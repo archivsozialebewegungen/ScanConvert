2eb1a577340973a2593c51875e0e48f64fead2fd
Michael Koltan <admin@archivsozialebewegungen.de>
2021-04-05 11:50:27 +0200

    Konvertierung von Bildrepräsentation in eigene Klasse ausgelagert

---
+++ src/Asb/ScanConverter/ImageTypeConversion.py
@@ -0,0 +1,49 @@
+'''
+Created on 05.04.2021
+
+@author: michael
+'''
+
+from PIL import Image
+import cv2
+from numpy import ndarray
+import numpy
+
+
+def pil_to_skimage(img: Image) -> ndarray:
+
+    return pil_to_ndarray(img)
+
+def skimage_to_pil(ndarray: ndarray, resolution: (int,)) -> Image:
+    
+    return ndarray_to_pil(ndarray, resolution)
+
+def pil_to_ndarray(img: Image) -> ndarray:
+    
+    return numpy.array(img)
+
+def ndarray_to_pil(ndarray: ndarray, resolution: (int,)) -> Image:
+    
+    img = Image.fromarray(ndarray)
+    img.info['dpi'] = resolution
+    return img
+
+def pil_to_rgb_cv2image(img: Image):
+    
+    return pil_to_ndarray(img)
+    
+def rgb_cv2image_to_pil(cv2_image, resolution: (int,)):
+    
+    return ndarray_to_pil(cv2_image, resolution)
+
+def pil_to_native_cv2image(img: Image):
+    
+    if img.mode == 'L' or img.mode == '1':
+        return pil_to_ndarray(img)
+    return cv2.cvtColor(pil_to_ndarray(img), cv2.COLOR_RGB2BGR)
+
+def native_cv2image_to_pil(cv2_image, resolution: (int,)):
+    
+    if len(cv2_image.shape) < 3:
+        return ndarray_to_pil(cv2_image, resolution)
+    return ndarray_to_pil(cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB), resolution)
---
+++ test/Asb/ScanConverter/ConversionTests.py
@@ -0,0 +1,61 @@
+'''
+Created on 04.04.2021
+
+@author: michael
+'''
+from Asb.ScanConverter.ImageOperations import ImageFileOperations
+from Asb.ScanConverter.ImageTypeConversion import \
+    pil_to_ndarray, ndarray_to_pil, \
+    pil_to_native_cv2image, native_cv2image_to_pil, pil_to_rgb_cv2image, \
+    rgb_cv2image_to_pil
+from PIL import ImageChops
+import os
+import unittest
+
+class ConversionTest(unittest.TestCase):
+
+
+    def setUp(self):
+        
+        self.ops = ImageFileOperations()
+        # This is a color, 600 dpi image
+        self.color = self.ops.load_image(os.path.join("Images", "RoterStern2.jpg"))
+        self.gray = self.ops.load_image(os.path.join("Images", "Image1.png"))
+        self.bw = self.ops.binarization_otsu(self.gray)
+        
+        self.images = ((self.color, 600), (self.gray, 300), (self.bw, 300))
+
+
+    def testToNumpyAndBack(self):
+        
+        for img in self.images:
+            with self.subTest():
+                ndarray = pil_to_ndarray(img[0])
+                copy = ndarray_to_pil(ndarray, (img[1], img[1]))
+                diff = ImageChops.difference(img[0], copy)
+                self.assertFalse(diff.getbbox())
+                self.assertEqual((img[1], img[1]), self.ops.get_resolution(copy))
+    
+    def testToNativeCV2AndBack(self):
+        
+        for img in self.images:
+            with self.subTest():
+                cv2img = pil_to_native_cv2image(img[0])
+                copy = native_cv2image_to_pil(cv2img, (img[1], img[1]))
+                diff = ImageChops.difference(img[0], copy)
+                self.assertFalse(diff.getbbox())
+                self.assertEqual((img[1], img[1]), self.ops.get_resolution(copy))
+
+    def testToRGBCV2AndBack(self):
+        
+        for img in self.images:
+            with self.subTest():
+                cv2img = pil_to_rgb_cv2image(img[0])
+                copy = rgb_cv2image_to_pil(cv2img, (img[1], img[1]))
+                diff = ImageChops.difference(img[0], copy)
+                self.assertFalse(diff.getbbox())
+        self.assertEqual((img[1], img[1]), self.ops.get_resolution(copy))
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
